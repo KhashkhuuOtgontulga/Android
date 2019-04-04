650091e248056b7d6c89710c72d5310da5008944
bolortuya <btumurbaatar@hawk.iit.edu>
2019-04-03 23:05:11 -0500

    Version 3
    added an image on top of the image to know that you are adding a picture. next I need to add the backarrow icon and functionality.

--- Rewards/app/src/main/java/com/example/rewards/CreateProfile.java
+++ Rewards/app/src/main/java/com/example/rewards/CreateProfile.java
@@ -6,8 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -15,12 +21,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Locale;
 
 public class CreateProfile extends AppCompatActivity {
 
     private int REQUEST_IMAGE_GALLERY = 1;
+    private int REQUEST_IMAGE_CAPTURE = 2;
     public static final String extraName = "DATA HOLDER";
     public static int MAX_CHARS = 360;
 
@@ -33,6 +47,9 @@ public class CreateProfile extends AppCompatActivity {
     private EditText position;
     private EditText story;
     private TextView charCountText;
+
+    private ImageView imageView;
+    private File currentImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,11 @@ public class CreateProfile extends AppCompatActivity {
 
         story.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_CHARS)});
         addTextListener();
+
+        imageView = findViewById(R.id.profileImage);
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
     }
 
     private void addTextListener() {
@@ -122,12 +144,95 @@ public class CreateProfile extends AppCompatActivity {
         }
     }
 
+    public void pickOption(final View w) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.logo);
+        builder.setTitle("Profile Picture");
+        builder.setMessage("Take picture from: ");
+        builder.setPositiveButton("Camera",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        doCamera(w);
+                    }
+                }
+        );
+        builder.setNegativeButton("Gallery",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        doGallery(w);
+                    }
+                }
+        );
+        builder.setNeutralButton("Cancel",
+                new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        dialog.dismiss();
+                    }
+                }
+        );
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     public void doGallery(View v) {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("image/*");
         startActivityForResult(photoPickerIntent, REQUEST_IMAGE_GALLERY);
     }
 
+    public void doCamera(View v) {
+        currentImageFile = new File(getExternalCacheDir(), "appimage_" + System.currentTimeMillis() + ".jpg");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(currentImageFile));
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            try {
+                processGallery(data);
+            } catch (Exception e) {
+                Toast.makeText(this, "onActivityResult: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                processCamera();
+            } catch (Exception e) {
+                Toast.makeText(this, "onActivityResult: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void processCamera() {
+        Uri selectedImage = Uri.fromFile(currentImageFile);
+        imageView.setImageURI(selectedImage);
+        Bitmap bm = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
+        currentImageFile.delete();
+    }
+
+    private void processGallery(Intent data) {
+        Uri galleryImageUri = data.getData();
+        if (galleryImageUri == null)
+            return;
+
+        InputStream imageStream = null;
+        try {
+            imageStream = getContentResolver().openInputStream(galleryImageUri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+    }
 
     public static void makeCustomToast(Context context, int time) {
         Toast toast = Toast.makeText(context, "User Create Successful", time);
