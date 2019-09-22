f3ab0d74c219191e93d8352130a667d22124b51a
Khashkhuu Otgontulga <kotgontulga@hawk.iit.edu>
2019-09-21 20:30:25 -0500

    Adding Dummy Functionality

---
+++ calculatorlibrary/src/main/java/com/example/calculatorlibrary/CoreFunctionality.java
@@ -0,0 +1,13 @@
+package com.example.calculatorlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class CoreFunctionality {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
